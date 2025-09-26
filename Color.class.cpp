de3e4d1a7516fbeead2e245fc59a51c7650eb63f
Phillip Wagner <bigbluepanda7@gmail.com>
2025-09-26 12:08:21 -0700

    Third addition to Color.class.cpp

--- Color.class.cpp
+++ Color.class.cpp
@@ -12,9 +12,21 @@ class Color {
 
     Color(int r, int b, int g): red(r), blue(b), green(g) {}
 }
+
+voide print()
+
+
 int main() {
 
+    Color c1();
+    Color c2();
+    Color c3();
+    Color c4();
 
+
+    c1.setRed(128);
+    c2.setBlue(192);
+    c3.setGreen(64);
 
 
 
