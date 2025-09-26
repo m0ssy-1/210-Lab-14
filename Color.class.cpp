40c68a8162ce7d6fc8fa3fca87a5c4eb26e69672
Phillip Wagner <bigbluepanda7@gmail.com>
2025-09-25 18:13:15 -0700

    Second addition to color.class.cpp

--- Color.class.cpp
+++ Color.class.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
 
+class Color {
+    private:
+    int red;
+    int blue;
+    int green;
+
+    public:
+    Color(): red(0), blue(0), green(0), {}
+
+    Color(int r, int b, int g): red(r), blue(b), green(g) {}
+}
 int main() {
 
 
